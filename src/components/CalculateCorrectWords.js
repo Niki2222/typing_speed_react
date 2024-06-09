26f272299d611723beb573a1aa50518e1e9d5037
Niculina <virtual_n_r@yahoo.com>
2024-06-09 19:42:49 +0300

    split calculateWords function

--- src/components/CalculateCorrectWords.js
+++ src/components/CalculateCorrectWords.js
@@ -1,13 +1,36 @@
-export default function CalculateCorrectWords({paragraph, text, getCorrectWords}) {
-    const words = text.split(' ');
-    const paragraphWords = paragraph.split(' ');
-    let correctCount = 0;
+export default function CalculateCorrectWords(props) {
+    const {paragraph, inputText, setInputText, setIsTimerStarted, 
+        setCorrectWords, isTimerStarted, timer} = props;
 
-    words.forEach((word, index) => {
-      if (word === paragraphWords[index]) {
-        ++correctCount;
-      }
-    });
-    console.log(correctCount);
-    getCorrectWords(correctCount);
-  }
+    function handleInputChange(e) {
+        if (!isTimerStarted) {
+            setIsTimerStarted(true);
+        }
+        setInputText(e.target.value);
+        calculateCorrectWords(e.target.value);
+    }
+
+    function calculateCorrectWords(text) {
+        const words = text.split(' ');
+        const paragraphWords = paragraph.split(' ');
+        let correctCount = 0;
+
+        words.forEach((word, index) => {
+            if (word === paragraphWords[index]) {
+            ++correctCount;
+            }
+        });
+        setCorrectWords(correctCount);
+    }
+
+    return (
+        <textarea 
+            cols="60" 
+            rows="10" 
+            value={inputText}
+            onChange={handleInputChange}
+            disabled={timer === 0}
+            autoFocus
+        ></textarea>
+    );
+}
--- src/components/StartGame.js
+++ src/components/StartGame.js
@@ -25,31 +25,6 @@ export default function StartGame({ paragraph, resetGame }) {
     return () => clearInterval(interval);
   }, [isTimerStarted]);
 
-//   useEffect(() => {
-//     function getCorrectWords(number) {
-//         setCorrectWords(number);
-//         console.log(number);
-//         return number;
-//     }
-//     <CalculateCorrectWords paragraph={paragraph} text={inputText}
-//       getCorrectWords={getCorrectWords} />
-//   }, [inputText])
-
-  function handleInputChange(e) {
-    if (!isTimerStarted) {
-      setIsTimerStarted(true);
-    }
-    setInputText(e.target.value);
-    <CalculateCorrectWords paragraph={paragraph} text={inputText}
-      getCorrectWords={getCorrectWords} />
-  }
-
-  function getCorrectWords(number) {
-    
-    // console.log(number);
-    return number;
-  }
-
   return (
     <div id='testing'>
       <h2>Timer: {timer} seconds</h2>
@@ -57,14 +32,15 @@ export default function StartGame({ paragraph, resetGame }) {
       <p id='text'>
         <RenderParagraph paragraph={paragraph} inputText={inputText} />
       </p>
-      <textarea 
-        cols="60" 
-        rows="10" 
-        value={inputText}
-        onChange={handleInputChange}
-        disabled={timer === 0}
-        autoFocus
-      ></textarea>
+      <CalculateCorrectWords 
+        paragraph={paragraph} 
+        inputText={inputText}
+        isTimerStarted={isTimerStarted}
+        timer={timer}
+        setInputText={setInputText}
+        setCorrectWords={setCorrectWords} 
+        setIsTimerStarted={setIsTimerStarted}
+        />
       <div>
         {timer === 0 && <button onClick={resetGame}>Restart</button>}
       </div>
